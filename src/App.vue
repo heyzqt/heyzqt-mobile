e778aa56a976bda281bba9858cc74b8218621698
zhangqing <heyzqt@qq.com>
2020-02-15 18:54:29 +0800

    screen1中3个元素的动画完成

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,57 @@
         <div class="header__nav-item_buy_button">立即购买</div>
       </div>
     </div>
+    <div class="right-nav">
+      <a v-for="(item,index) in navList" :key="index" href="javascript:" class="item" 
+      :class="{'active' : tabIndex==index}" ref="navItem"
+      @click="changeCurrentTab(index)">{{item}}</a>
+    </div>
+    <div class="screen-1">
+      <div class="screen-1__heading">
+        <b>heyzqt手机</b>
+        让你的生活更精彩
+      </div>
+      <div class="screen-1__phone"></div>
+      <div class="screen-1__shadow"></div>
+    </div>
   </div>
 </template>
 <script>
+var screenAnimateElements = {
+  '.screen-1' : [
+    '.screen-1__heading',
+    '.screen-1__phone',
+    '.screen-1__shadow',
+  ],
+  '.screen-2' : [
+    '.screen-2__heading',
+    '.screen-2__subheading',
+    '.screen-2__phone',
+    '.screen-2__point_i_1',
+    '.screen-2__point_i_2',
+    '.screen-2__point_i_3',
+  ],
+  '.screen-3' : [
+    '.screen-3__heading',
+    '.screen-3__phone',
+    '.screen-3__subheading',
+    '.screen-3__features',
+  ],
+  '.screen-4' : [
+    '.screen-4__heading',
+    '.screen-4__subheading',
+    '.screen-4__type__item_i_1',
+    '.screen-4__type__item_i_2',
+    '.screen-4__type__item_i_3',
+    '.screen-4__type__item_i_4',
+  ],
+  '.screen-5' : [
+     '.screen-5__heading',
+    '.screen-5__subheading',
+    '.screen-5__bg',
+  ]
+
+};
 import storage from './utils/storage'
 export default {
   data() {
@@ -29,18 +77,55 @@ export default {
       this.tabIndex = storage.get('tabIndex');
     }
     this.scrollLeft=this.$refs.navItem[this.tabIndex].offsetLeft;
+
+    //screen1的动画
+    this.setScreenAnimateInit('.screen-1__heading');
+    this.setScreenAnimateInit('.screen-1__phone');
+    this.setScreenAnimateInit('.screen-1__shadow');
+  //   for(let k in screenAnimateElements){
+  //     if(k == '.screen-1'){
+  //       continue;
+  //     }
+  //     setScreenAnimateInit(k);
+  //  }
+    setTimeout(()=>{
+      this.playScreenAnimateDone('.screen-1__heading');
+      this.playScreenAnimateDone('.screen-1__phone');
+      this.playScreenAnimateDone('.screen-1__shadow');
+    }, 100)
+    
   },
   methods: {
+    setScreenAnimateInit(screenCls) {
+      var screen = document.querySelector(screenCls); // 获取当前屏的元素
+      // var animateElements =  screenAnimateElements[screenCls]; // 需要设置动画的元素
+      // for(var i=0;i<animateElements.length;i++){
+      //     var element = document.querySelector(animateElements[i]);
+      //     var baseCls = element.getAttribute('class');
+      //     element.setAttribute('class',baseCls +' '+animateElements[i].substr(1)+'_animate_init');
+      // }
+
+      let baseCls = screen.getAttribute('class');
+      screen.setAttribute('class', baseCls+" "+screenCls.substring(1)+'_animate_init'); 
+    },
+    playScreenAnimateDone(screenCls){
+        var screen = document.querySelector(screenCls); // 获取当前屏的元素
+        // var animateElements =  screenAnimateElements[screenCls]; // 需要设置动画的元素
+        // for(var i=0;i<animateElements.length;i++){
+        //     var element = document.querySelector(animateElements[i]);
+        //     var baseCls = element.getAttribute('class');
+        //     element.setAttribute('class',baseCls.replace('_animate_init','_animate_done'));    
+        // }
+        let baseCls = screen.getAttribute('class');
+        screen.setAttribute('class', baseCls.replace('_animate_init', '_animate_done')); 
+    },
     changeIndicator(index) {
-      console.log(this.$refs.navItem[index].offsetLeft);
-      console.dir(this.$refs.navTip);
-      console.dir(this.$refs.navTip.style);
       this.scrollLeft=this.$refs.navItem[index].offsetLeft;
-      console.log('scrollLeft=', this.scrollLeft);
     },
     changeCurrentTab(index) {
       this.tabIndex = index;
       storage.set('tabIndex', index);
+      this.scrollLeft=this.$refs.navItem[index].offsetLeft;
     }
   }
 }
